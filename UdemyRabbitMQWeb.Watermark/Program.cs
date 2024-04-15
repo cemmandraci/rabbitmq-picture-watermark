4c43427cbcebf9671955b195b195a029f71b7543
Cem Mandraci <Cem.Mandraci@bs.nttdata.com>
2024-04-15 16:45:41 +0300

    add in memory database config settings

--- UdemyRabbitMQWeb.Watermark/Program.cs
+++ UdemyRabbitMQWeb.Watermark/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using UdemyRabbitMQWeb.Watermark.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseInMemoryDatabase(databaseName: "productDb");
+});
 
 var app = builder.Build();
 
@@ -12,6 +19,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
